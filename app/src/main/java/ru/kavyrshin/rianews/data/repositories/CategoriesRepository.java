2ba0e6b2f3548784904d8c72473159f78b53d9c0
Roman <rom.kawirshin@gmail.com>
2018-03-21 02:49:01 +0300

    Добавлен ICategoriesRepository CategoriesRepository

---
+++ app/src/main/java/ru/kavyrshin/rianews/data/repositories/CategoriesRepository.java
@@ -0,0 +1,24 @@
+package ru.kavyrshin.rianews.data.repositories;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import io.reactivex.Single;
+import ru.kavyrshin.rianews.domain.global.models.Category;
+import ru.kavyrshin.rianews.domain.global.repositories.ICategoriesRepository;
+
+public class CategoriesRepository implements ICategoriesRepository {
+
+    @Override
+    public Single<List<Category>> getAllCategories() {
+        List<Category> categories = new ArrayList<>();
+        categories.add(new Category("First", "first.com"));
+        categories.add(new Category("Second", "second.com"));
+        categories.add(new Category("Third", "third.com"));
+        categories.add(new Category("Fourth", "fourth.com"));
+        categories.add(new Category("Fifth", "fifth.com"));
+        categories.add(new Category("Sixth", "sixth.com"));
+        return Single.just(categories);
+    }
+}
---
+++ app/src/main/java/ru/kavyrshin/rianews/domain/global/repositories/ICategoriesRepository.java
@@ -0,0 +1,10 @@
+package ru.kavyrshin.rianews.domain.global.repositories;
+
+import java.util.List;
+
+import io.reactivex.Single;
+import ru.kavyrshin.rianews.domain.global.models.Category;
+
+public interface ICategoriesRepository {
+    Single<List<Category>> getAllCategories();
+}
