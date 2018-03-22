c9d703550730c14c0e15da993b5cde54ec261ae9
Roman <rom.kawirshin@gmail.com>
2018-03-22 15:14:34 +0300

    Изменена реализация метода в NewsRepository

--- app/src/main/java/ru/kavyrshin/rianews/data/repositories/NewsRepository.java
+++ app/src/main/java/ru/kavyrshin/rianews/data/repositories/NewsRepository.java
@@ -1,12 +1,12 @@
 package ru.kavyrshin.rianews.data.repositories;
 
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import io.reactivex.Single;
+import ru.kavyrshin.rianews.data.network.JSoupRiaNews;
 import ru.kavyrshin.rianews.domain.global.models.Category;
 import ru.kavyrshin.rianews.domain.global.models.News;
 import ru.kavyrshin.rianews.domain.global.repositories.INewsRepository;
@@ -14,23 +14,16 @@ import ru.kavyrshin.rianews.domain.global.repositories.INewsRepository;
 public class NewsRepository implements INewsRepository {
 
 
+    private JSoupRiaNews parser;
+
     @Inject
-    public NewsRepository() {
+    public NewsRepository(JSoupRiaNews parser) {
+        this.parser = parser;
     }
 
     @Override
     public Single<List<News>> getNewsByCategory(Category category) {
-        List<News> newsList = new ArrayList<>();
-
-        for (int i = 0; i < 10; i++) {
-            News news = new News();
-            news.setImgUrl("https://pp.userapi.com/c639420/v639420066/54873/ZGGvQbaHdso.jpg");
-            news.setCategory(category);
-            news.setName(category.getName() + " " + i + " news");
-            newsList.add(news);
-        }
-
-        return Single.just(newsList);
+        return parser.getNewsByCategory(category);
     }
 
 }
