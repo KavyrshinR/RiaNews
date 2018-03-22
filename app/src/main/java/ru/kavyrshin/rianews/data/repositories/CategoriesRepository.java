6dad102e445752b9d40be1bc858d231852cf1f40
Roman <rom.kawirshin@gmail.com>
2018-03-22 03:04:50 +0300

    Изменена реализация метода getAllCategories

--- app/src/main/java/ru/kavyrshin/rianews/data/repositories/CategoriesRepository.java
+++ app/src/main/java/ru/kavyrshin/rianews/data/repositories/CategoriesRepository.java
@@ -1,30 +1,26 @@
 package ru.kavyrshin.rianews.data.repositories;
 
 
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import io.reactivex.Single;
+import ru.kavyrshin.rianews.data.network.JSoupRiaNews;
 import ru.kavyrshin.rianews.domain.global.models.Category;
 import ru.kavyrshin.rianews.domain.global.repositories.ICategoriesRepository;
 
 public class CategoriesRepository implements ICategoriesRepository {
 
+    private JSoupRiaNews jSoupRiaNews;
+
     @Inject
-    public CategoriesRepository() {
+    public CategoriesRepository(JSoupRiaNews jSoupRiaNews) {
+        this.jSoupRiaNews = jSoupRiaNews;
     }
 
     @Override
     public Single<List<Category>> getAllCategories() {
-        List<Category> categories = new ArrayList<>();
-        categories.add(new Category("First", "first.com"));
-        categories.add(new Category("Second", "second.com"));
-        categories.add(new Category("Third", "third.com"));
-        categories.add(new Category("Fourth", "fourth.com"));
-        categories.add(new Category("Fifth", "fifth.com"));
-        categories.add(new Category("Sixth", "sixth.com"));
-        return Single.just(categories);
+        return jSoupRiaNews.getAllCategories();
     }
 }
