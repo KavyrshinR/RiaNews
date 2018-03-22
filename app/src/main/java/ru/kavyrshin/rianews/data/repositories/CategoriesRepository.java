1d6f51097e9e77fac717a7df6c14f9941f458937
Roman <rom.kawirshin@gmail.com>
2018-03-22 06:39:00 +0300

    Добавлен и реализован метод в ICategoriesRepository

--- app/src/main/java/ru/kavyrshin/rianews/data/repositories/CategoriesRepository.java
+++ app/src/main/java/ru/kavyrshin/rianews/data/repositories/CategoriesRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.inject.Inject;
 
 import io.reactivex.Single;
+import io.reactivex.functions.Function;
 import ru.kavyrshin.rianews.data.network.JSoupRiaNews;
 import ru.kavyrshin.rianews.domain.global.models.Category;
 import ru.kavyrshin.rianews.domain.global.repositories.ICategoriesRepository;
@@ -14,6 +15,8 @@ public class CategoriesRepository implements ICategoriesRepository {
 
     private JSoupRiaNews jSoupRiaNews;
 
+    private List<Category> categories;
+
     @Inject
     public CategoriesRepository(JSoupRiaNews jSoupRiaNews) {
         this.jSoupRiaNews = jSoupRiaNews;
@@ -21,6 +24,27 @@ public class CategoriesRepository implements ICategoriesRepository {
 
     @Override
     public Single<List<Category>> getAllCategories() {
-        return jSoupRiaNews.getAllCategories();
+        return jSoupRiaNews.getAllCategories()
+                .map(new Function<List<Category>, List<Category>>() {
+                    @Override
+                    public List<Category> apply(List<Category> categories) throws Exception {
+                        for (int i = 0; i < categories.size(); i++) {
+                            categories.get(i).setId(i);
+                        }
+                        CategoriesRepository.this.categories = categories;
+                        return categories;
+                    }
+        });
+    }
+
+    @Override
+    public Single<Category> getCategoryById(int id) {
+        Category result = null;
+        for (Category category : categories) {
+            if (category.getId() == id) {
+                result = category;
+            }
+        }
+        return Single.just(result);
     }
 }
--- app/src/main/java/ru/kavyrshin/rianews/domain/global/repositories/ICategoriesRepository.java
+++ app/src/main/java/ru/kavyrshin/rianews/domain/global/repositories/ICategoriesRepository.java
@@ -7,4 +7,5 @@ import ru.kavyrshin.rianews.domain.global.models.Category;
 
 public interface ICategoriesRepository {
     Single<List<Category>> getAllCategories();
+    Single<Category> getCategoryById(int id);
 }
