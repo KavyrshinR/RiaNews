f28ff25f1de3d650636e003b7168b0323394cc02
Roman <rom.kawirshin@gmail.com>
2018-03-22 15:14:16 +0300

    Реализован парсинг списков новостей всех категорий

--- app/src/main/java/ru/kavyrshin/rianews/data/network/JSoupRiaNews.java
+++ app/src/main/java/ru/kavyrshin/rianews/data/network/JSoupRiaNews.java
@@ -11,6 +11,7 @@ import java.util.concurrent.Callable;
 
 import io.reactivex.Single;
 import ru.kavyrshin.rianews.domain.global.models.Category;
+import ru.kavyrshin.rianews.domain.global.models.News;
 
 public class JSoupRiaNews {
 
@@ -26,15 +27,63 @@ public class JSoupRiaNews {
                 for (Element element : categoriesHtml) {
                     Category category = new Category();
                     category.setName(element.select("a[href]").first().select("span").first().text());
-                    category.setUrl("http://ria.ru" + element.select("a[href]").first().attr("href"));
+                    String url = element.select("a[href]").first().attr("href");
+                    category.setUrl(url);
+                    if (!url.contains("ria.ru")) {
+                        category.setUrl("http://ria.ru" + url);
+                    }
 
                     if (!category.getUrl().contains("rsport")) {
                         categories.add(category);
                     }
                 }
-                categories.get(0).setUrl(categories.get(0) + "lenta");
+                categories.get(0).setUrl(categories.get(0).getUrl() + "lenta");
 
                 return categories;
+            }
+        });
+    }
+
+    public Single<List<News>> getNewsByCategory(final Category category) {
+        return Single.fromCallable(new Callable<List<News>>() {
+            @Override
+            public List<News> call() throws Exception {
+                List<News> newsList = new ArrayList<>();
+                Document categoryHtml = Jsoup.connect(category.getUrl()).get();
+                Element contentHtml = categoryHtml.selectFirst("div.b-list");
+                if (contentHtml != null) {
+                    Elements elements = contentHtml.select("div.b-list__item");
+
+                    for (Element element : elements) {
+                        News news = new News();
+                        news.setImgUrl(element.selectFirst("img").attr("src"));
+                        news.setCategory(category);
+                        news.setName(element.selectFirst("span.b-list__item-title").text());
+                        news.setUrl("http://ria.ru" + element.selectFirst("a[href]").attr("href"));
+
+                        newsList.add(news);
+                    }
+
+                } else {
+                    contentHtml = categoryHtml.selectFirst("div.b-themespec__tiles-list").selectFirst("div.owl-carousel");
+                    Elements elements = contentHtml.select("div.b-themespec__tiles-item");
+
+                    for (Element element : elements) {
+                        News news = new News();
+                        news.setImgUrl(element.selectFirst("img").attr("src"));
+                        news.setCategory(category);
+                        news.setName(element.selectFirst("span.b-themespec__tiles-item-title").selectFirst("span").text());
+                        String url = element.selectFirst("a.b-themespec__tiles-item-link").attr("href");
+                        news.setUrl(url);
+                        if (!url.contains("ria.ru")) {
+                            news.setUrl("http://ria.ru" + url);
+                        }
+
+                        newsList.add(news);
+                    }
+                }
+
+                return newsList;
             }
         });
     }
