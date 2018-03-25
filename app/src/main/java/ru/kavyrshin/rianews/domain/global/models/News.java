0933e7cfa25e9f5a76dd2f80b2c857209cda7fc5
Roman <rom.kawirshin@gmail.com>
2018-03-26 01:49:39 +0300

    Добавлены новые переменные в News

--- app/src/main/java/ru/kavyrshin/rianews/domain/global/models/News.java
+++ app/src/main/java/ru/kavyrshin/rianews/domain/global/models/News.java
@@ -5,8 +5,9 @@ public class News {
     private int id;
     private String url;
     private String name;
-    private int unixtime;
+    private long unixtime;
     private String imgUrl;
+    private String bigImgUrl;
     private Category category;
     private String text;
 
@@ -34,11 +35,11 @@ public class News {
         this.name = name;
     }
 
-    public int getUnixtime() {
+    public long getUnixtime() {
         return unixtime;
     }
 
-    public void setUnixtime(int unixtime) {
+    public void setUnixtime(long unixtime) {
         this.unixtime = unixtime;
     }
 
@@ -48,6 +49,14 @@ public class News {
 
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
+    }
+
+    public String getBigImgUrl() {
+        return bigImgUrl;
+    }
+
+    public void setBigImgUrl(String bigImgUrl) {
+        this.bigImgUrl = bigImgUrl;
     }
 
     public Category getCategory() {
